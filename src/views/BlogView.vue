c7c279a172bf37fdb5e4a6c20e00decbf591a31c
RATACIA <134484195+RATACIA@users.noreply.github.com>
2024-03-14 14:56:53 +0000

    embed for fb links

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="container">
-    <div class="about">
-      <h1>This is an Blogs page</h1>
-      <h1>This is an Blogs page</h1>
-      <h1>This is an Blogs page</h1>
-      <h1>This is an Blogs page</h1>
-      <h1>This is an Blogs page</h1>
-    </div>
+    <iframe
+      src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid0ke3mMaWcYwkrxm9NrJ5bSQdxApKKvfBu6UJYxE84ZByP8JJBvDUknPqr16dhZ45Jl%26id%3D100090998603012&show_text=true&width=500"
+      width="500"
+      height="601"
+      style="border: none; overflow: hidden"
+      scrolling="no"
+      frameborder="0"
+      allowfullscreen="true"
+      allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+    ></iframe>
+    <iframe
+      src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02bNQWLd3xm2R9Kqex5Bj3bYuUsjfz7GpHemmjD9vdmhAP9GCuSBE7aVmECSTyuCkNl%26id%3D100090998603012&show_text=true&width=500"
+      width="500"
+      height="767"
+      style="border: none; overflow: hidden"
+      scrolling="no"
+      frameborder="0"
+      allowfullscreen="true"
+      allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+    ></iframe>
   </div>
 </template>
 
