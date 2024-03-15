be910f61c45887608964f64d3595577d552d8390
RATACIA <134484195+RATACIA@users.noreply.github.com>
2024-03-15 22:49:20 +0000

    expasion panel to be used for this update in progress

--- src/components/ServicesComp.vue
+++ src/components/ServicesComp.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <v-row>
       <v-col>
@@ -36,6 +36,17 @@
       </v-col>
     </v-row>
   </div>
+</template> -->
+<template>
+  <div class="text-subtitle-2 mb-2">Default</div>
+  <v-expansion-panels>
+    <v-expansion-panel
+      v-for="i in 3"
+      :key="i"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      title="Item"
+    ></v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script setup>
