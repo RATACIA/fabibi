948cc261f5c5a50fe991a4bd9c5137b6b4cef186
RATACIA <134484195+RATACIA@users.noreply.github.com>
2024-03-19 21:16:32 +0000

    styling update for confirmation message, still wip

--- src/components/ThankYouComp.vue
+++ src/components/ThankYouComp.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="container">
+  <div class="ma-2">
     <div>
-      <v-card-title
-        >Thank you, {{ props.firstName }} {{ props.lastName }}, for your
-        message!</v-card-title
+      <span
+        ><v-icon class="mx-0 mb-1">mdi-ticket-confirmation</v-icon>Thank you,
+        {{ props.firstName }} {{ props.lastName }}, for your message!</span
       >
+      <br />
 
-      <v-card-subtitle
+      <span
         >We have received your message and we aim to reply to your messaged
-        within 24 hours.</v-card-subtitle
-      >
-      <v-card-text>Our working hours are: Mon - Sun: 11am - 7pm</v-card-text>
+        within 24 hours.</span
+      ><br />
+      <span>Our working hours are: Mon - Sun: 11am - 7pm</span>
+      <p class="pl-4">
+        For any urgent queries, please contact us via phone number:070000000
+      </p>
     </div>
   </div>
 </template>
@@ -20,11 +24,14 @@ import { defineProps } from "vue";
 const props = defineProps(["firstName", "lastName"]);
 </script>
 <style scoped>
-.container {
+/* .container {
   display: flex;
   justify-items: center;
   align-items: center;
   height: 100vh;
   width: 100%;
+} */
+span {
+  margin-left: 16px;
 }
 </style>
